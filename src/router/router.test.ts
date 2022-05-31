3b89dbdb51ffd9b533eaf2e9df25c8c84abbcf47
Dominik Linkowski <dominiklinq@gmail.com>
2022-05-31 18:49:37 +0200

    feat(router): router module

---
+++ src/router/router.test.ts
@@ -0,0 +1,36 @@
+import { assert, assertEquals } from 'https://deno.land/std@0.74.0/testing/asserts.ts';
+import { HandlerParams } from '../route/route.ts';
+import { Router } from './router.ts';
+
+Deno.test('The router', async (t: Deno.TestContext) => {
+  const router = new Router([
+    {
+      path: '/',
+      handler: () => new Response('response'),
+    },
+    {
+      path: '/:id/test/:name',
+      method: 'POST',
+      handler: (config: HandlerParams) =>
+        new Response(`${config.params.id}-${config.request.method}`),
+    },
+  ]);
+
+  await t.step('should match request', () => {
+    assert(router.match('/', 'GET')[0], 'The route should match the path');
+    assertEquals(
+      router.match('/', 'POST'),
+      [undefined, 405],
+      'The route should\'t match the path and return 405 error for bad method',
+    );
+    assertEquals(
+      router.match('/unknown/unknown', 'POST'),
+      [undefined, 404],
+      'The route should\'t match the path and return 404 error for bad path',
+    );
+    assert(
+      router.match('/1/test/adam', 'POST'),
+      'The route should match the path and method',
+    );
+  });
+});
---
+++ src/router/router.ts
@@ -0,0 +1,38 @@
+import { Route, RouteConfig, RouteMethod } from '../route/route.ts';
+
+/**
+ * Router class for handling requests and responses to the server.
+ */
+export class Router {
+  private routing: Route[];
+
+  /**
+   * Constructor.
+   * @param routes - array of route config objects.
+   */
+  constructor(routes: RouteConfig[]) {
+    this.routing = routes.map((route) => new Route(route));
+  }
+
+  /**
+   * Method to match the path.
+   *
+   * @param path - string to match
+   * @param method - method to match
+   * @returns object with params if the path matches.
+   */
+  public match(
+    path: string,
+    method: RouteMethod,
+  ): [Route | undefined, number | undefined] {
+    const route = this.routing.find((route) => route.match(path));
+
+    if (!route) return [undefined, 404];
+
+    if (!route.matchMethod(method)) {
+      return [undefined, 405];
+    }
+
+    return [route, undefined];
+  }
+}
